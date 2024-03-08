9ade7146a9a248d4607946919c020b90781034ba
ericayoung <eyoung135@gmail.com>
2024-03-08 15:56:04 -0500

    takes out components and adds objects now need to address passing through different data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
 
+const author='Abigail S.';
+const title = 'Bad Therapy: Why the Kids Arent Growing Up';
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/81RYRl7DvXL._AC_UL600_SR600,400_.jpg';
+
+
+
 const Booklist = () => {
-  return (
+    return (
     <section className='booklist'>
       <Book />
       <Book />
@@ -15,24 +22,13 @@ const Booklist = () => {
 
 const Book = () => {
   return (
-    <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img src= {img} alt={title}/>
+      <h2> {title} </h2>
+      <h4> {author.toUpperCase()} </h4>
     </article>
   )
-}
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81RYRl7DvXL._AC_UL600_SR600,400_.jpg"
-    alt="Bad Therapy: Why the Kids Arent Growing Up"
-  />
-)
-const Title = () => <h2> Bad Therapy: Why the Kids Aren't Growing Up </h2>
-const Author = () => {
-  return <h4> Abigail Shrier </h4>
-}
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 

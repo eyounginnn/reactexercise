019306181edeb9d00a71a49bf52d6ead8e3686df
ericayoung <eyoung135@gmail.com>
2024-03-08 11:28:19 -0500

    creating components of a book list and best seller books

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greeting() {
+const Booklist = () => {
   return (
-    <div>
-      <Person/>
-    <Message/>
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <h2> john doe </h2>
-const Message = () => { 
-    return <p> this is my message </p>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => <h2> image placeholder </h2>;
+const Title = () => <h2> Book Title </h2>;
+const Author = () => {
+  return <h4> Author </h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<Booklist />)
